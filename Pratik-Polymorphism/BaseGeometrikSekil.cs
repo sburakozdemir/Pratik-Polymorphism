a0d55ffa91d17583e924467274bf6eb32c4ffc66
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-08 13:00:47 +0300

    Add project files.

---
+++ Pratik-Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Pratik_Polymorphism
+{
+    // Bu sınıf, tüm geometrik şekiller için temel özellikleri ve davranışları içerir.
+    public abstract class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // Bu metod, tüm geometrik şekiller için alan hesaplamasını yapar.
+        // Ancak, her şeklin kendi hesaplama mantığı olduğundan, bu metod soyut (abstract) olarak tanımlanır.
+        public abstract void AlanHesapla();
+    }
+
+    // Kare sınıfı, BaseGeometrikSekil sınıfından türetilir.
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        // Kare sınıfının kendi alan hesaplama mantığını override eder.
+        public override void AlanHesapla()
+        {
+            Console.WriteLine($"Kare Alanı: {Genislik * Yukseklik}");
+        }
+    }
+
+    // Dikdörtgen sınıfı, BaseGeometrikSekil sınıfından türetilir.
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        // Dikdörtgen sınıfının kendi alan hesaplama mantığını override eder.
+        public override void AlanHesapla()
+        {
+            Console.WriteLine($"Dikdörtgen Alanı: {Genislik * Yukseklik}");
+        }
+    }
+
+    // Dik Üçgen sınıfı, BaseGeometrikSekil sınıfından türetilir.
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public DikUcgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        // Dik Üçgen sınıfının kendi alan hesaplama mantığını override eder.
+        public override void AlanHesapla()
+        {
+            Console.WriteLine($"Dik Üçgen Alanı: {(Genislik * Yukseklik) / 2}");
+        }
+    }
+}
---
+++ Pratik-Polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using Pratik_Polymorphism;
+
+// Kare nesnesi oluşturulur ve alan hesaplaması yapılır.
+Kare kare = new Kare(10, 10);
+kare.AlanHesapla();
+
+// Dikdörtgen nesnesi oluşturulur ve alan hesaplaması yapılır.
+Dikdortgen dikdortgen = new Dikdortgen(10, 20);
+dikdortgen.AlanHesapla();
+
+// Dik Üçgen nesnesi oluşturulur ve alan hesaplaması yapılır.
+DikUcgen dikUcgen = new DikUcgen(48, 40);
+dikUcgen.AlanHesapla();
